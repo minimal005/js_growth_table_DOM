76b595e38848df716f1014e6796f01d5fbedef5b
Halyna <minimal005@gmail.com>
2024-10-18 23:19:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,9 @@ const rowAdd = document.querySelector('.append-row');
 const columnAdd = document.querySelector('.append-column');
 const columnRemove = document.querySelector('.remove-column');
 
+const MIN_VALUE = 2;
+const MAX_VALUE = 10;
+
 document.addEventListener('click', (e) => {
   const target = e.target.closest('.button');
 
@@ -22,55 +25,51 @@ function changeTable(elem) {
   const removeRow = elem.closest('.remove-row');
   const removeColumn = elem.closest('.remove-column');
 
-  switch (elem) {
-    case addRow:
-      const cloneElem = table.rows[0].cloneNode(true);
+  if (addRow) {
+    const cloneElem = table.rows[0].cloneNode(true);
 
-      table.prepend(cloneElem);
+    table.append(cloneElem);
 
-      if (table.rows.length === 10) {
-        addAttribute(addRow);
-      }
-      break;
+    if (table.rows.length === MAX_VALUE) {
+      addAttribute(addRow);
+    }
 
-    case removeRow:
-      table.rows[table.rows.length - 1].remove();
-
-      if (table.rows.length === 2) {
-        addAttribute(removeRow);
-      }
-
-      break;
-
-    case addColumn:
-      [...table.rows].forEach((el) => {
-        const td = document.createElement('td');
-
-        el.prepend(td);
-      });
-
-      if (table.rows[0].cells.length === 10) {
-        addAttribute(addColumn);
-      }
-
-      break;
-
-    case removeColumn:
-      [...table.rows].forEach((el) => {
-        el.cells[el.cells.length - 1].remove();
-      });
-
-      if (table.rows[0].cells.length === 2) {
-        addAttribute(removeColumn);
-      }
-
-      break;
-
-    default:
-      break;
+    check();
   }
 
-  check();
+  if (removeRow) {
+    table.rows[table.rows.length - 1].remove();
+
+    if (table.rows.length === MIN_VALUE) {
+      addAttribute(removeRow);
+    }
+
+    check();
+  }
+
+  if (addColumn) {
+    [...table.rows].forEach((el) => {
+      const td = document.createElement('td');
+
+      el.append(td);
+    });
+
+    if (table.rows[0].cells.length === MAX_VALUE) {
+      addAttribute(addColumn);
+    }
+    check();
+  }
+
+  if (removeColumn) {
+    [...table.rows].forEach((el) => {
+      el.cells[el.cells.length - 1].remove();
+    });
+
+    if (table.rows[0].cells.length === MIN_VALUE) {
+      addAttribute(removeColumn);
+    }
+    check();
+  }
 }
 
 function addAttribute(el) {
@@ -81,19 +80,18 @@ function removeAttribute(el) {
   el.removeAttribute('disabled');
 }
 
-// checking table sizes and activating inactive buttons
 function check() {
-  if (table.rows.length > 2 && rowRemove.hasAttribute('disabled')) {
+  if (table.rows.length > MIN_VALUE && rowRemove.hasAttribute('disabled')) {
     removeAttribute(rowRemove);
-  } else if (table.rows.length < 10 && rowAdd.hasAttribute('disabled')) {
+  } else if (table.rows.length < MAX_VALUE && rowAdd.hasAttribute('disabled')) {
     removeAttribute(rowAdd);
   } else if (
-    table.rows[0].cells.length > 2 &&
+    table.rows[0].cells.length > MIN_VALUE &&
     columnRemove.hasAttribute('disabled')
   ) {
     removeAttribute(columnRemove);
   } else if (
-    table.rows[0].cells.length < 10 &&
+    table.rows[0].cells.length < MAX_VALUE &&
     columnAdd.hasAttribute('disabled')
   ) {
     removeAttribute(columnAdd);
